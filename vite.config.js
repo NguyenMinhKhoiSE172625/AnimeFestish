c3c8420002f9ef95fd4a555d407f7b5e1d19ef85
NguyenMinhKhoiSE172625 <khoinmse172625@fpt.edu.vn>
2026-03-10 03:06:35 +0700

    23

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,24 @@ import { defineConfig } from "vite";
 export default defineConfig({
   build: {
     chunkSizeWarningLimit: 600,
+    sourcemap: false,
+    minify: 'terser',
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+        passes: 2,
+      },
+      mangle: {
+        toplevel: true,
+        properties: {
+          regex: /^_/,
+        },
+      },
+      format: {
+        comments: false,
+      },
+    },
   },
   server: {
     proxy: {
